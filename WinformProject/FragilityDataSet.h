ed92b065328284376cf23ec675faa202174aaaae
ho deok park <jusjuno@naver.com>
2021-05-31 09:01:52 +0900

    취약성 곡선 함수 추가

--- WinformProject/FragilityDataSet.h
+++ WinformProject/FragilityDataSet.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "FragilityFile.h"
+#include "FileUtil.h"
+#include "CSVFileManager.h"
 
 using namespace System;
 using namespace System::Collections::Generic;
@@ -12,11 +14,18 @@ namespace WinformProject {
 		FragilityDataSet() {
 			this->m_FragilityFileDict = gcnew Dictionary<double, FragilityFile^>();
 		}
+
+		FragilityDataSet(String^ sFragilityCurvFileName) {
+			this->m_FragilityFileDict = gcnew Dictionary<double, FragilityFile^>();
+			setFragilityCurvFile(sFragilityCurvFileName);
+		}
+
+
 	public:
 		static Dictionary<String^, String^>^ m_FragilityCurvDict;
 		//static DataTable^                    m_FragilityCurvData;
 
-
+		static String^ sPath = "C:\\sample\\Fragility_Curve_Library\\";
 		Dictionary<double, FragilityFile^>^ m_FragilityFileDict;
 
 	public:
@@ -1097,6 +1106,59 @@ namespace WinformProject {
 			}
 		}
 
+
+		void setFragilityCurvFile(String^ sFragilityCurvFileName) {
+
+			CSVFileManager^ csv = gcnew CSVFileManager(this->sPath + sFragilityCurvFileName + ".csv");
+			String^ output = csv->Read();
+
+			if (!String::IsNullOrEmpty(output)) {
+				array<String^>^ dataArray = output->Split(Environment::NewLine->ToCharArray(), StringSplitOptions::RemoveEmptyEntries);
+				int outputRowCount = dataArray->Length;
+				Debug::WriteLine(String::Format("Number of output summary lines:{0}", outputRowCount));
+
+
+				bool isFind = false;
+				FragilityFile^ fragilityFile = nullptr;
+
+				for (int i = 0; i < outputRowCount; i++)
+				{
+					String^ outputLineData = dataArray[i];
+					Debug::WriteLine("=================>outputLineData:" + outputLineData);
+
+					double dPGA = 0;
+					double dSlight = 0;
+					double dModerate = 0;
+					double dSevere = 0;
+					double dCollapse = 0;
+
+					if (isFind) {
+						array<String^>^ values = CSVFileManager::Parse(outputLineData, ",");
+
+						Double::TryParse(values[0], dPGA);
+						Double::TryParse(values[1], dSlight);
+						Double::TryParse(values[2], dModerate);
+						Double::TryParse(values[3], dSevere);
+						Double::TryParse(values[4], dCollapse);
+
+						fragilityFile = gcnew FragilityFile();
+						fragilityFile->FileName = sFragilityCurvFileName;
+						fragilityFile->PGA = dPGA;
+						fragilityFile->Slight = dSlight;
+						fragilityFile->Moderate = dModerate;
+						fragilityFile->Severe = dSevere;
+						fragilityFile->Collapse = dCollapse;
+
+
+						this->m_FragilityFileDict->Add(dPGA, fragilityFile);
+					}
+
+					if (outputLineData->IndexOf("PGA") >= 0) {
+						isFind = true;
+					}
+				}
+			}
+		}
 		/*
 		static void setFragilityCurvData() {
 			FragilityDataSet::m_FragilityCurvData = gcnew DataTable();
--- WinformProject/Step4Form.h
+++ WinformProject/Step4Form.h
@@ -225,64 +225,9 @@ namespace WinformProject {
 			if (fragilityCurvDict->ContainsKey(selectComponentKey)) {
 				sFragilityCurvFileName = fragilityCurvDict[selectComponentKey];
 
-				String^ sPath = "C:\\sample\\Fragility_Curve_Library\\";
-
-				CSVFileManager^ csv = gcnew CSVFileManager(sPath + sFragilityCurvFileName +".csv");
-				String^ output = csv->Read();
-
-				FragilityDataSet^ fragilityDataSet = gcnew FragilityDataSet();
-
-				if (!String::IsNullOrEmpty(output)) {
-					array<String^>^ dataArray = output->Split(Environment::NewLine->ToCharArray(), StringSplitOptions::RemoveEmptyEntries);
-					int outputRowCount = dataArray->Length;
-					Debug::WriteLine(String::Format("Number of output summary lines:{0}", outputRowCount));
-
-
-					bool isFind = false;
-					FragilityFile^ fragilityFile = nullptr;
-					
-
-					for (int i = 0; i < outputRowCount; i++)
-					{
-						String^ outputLineData = dataArray[i];
-						Debug::WriteLine("=================>outputLineData:" + outputLineData);
-
-						double dPGA = 0;
-						double dSlight = 0;
-						double dModerate = 0;
-						double dSevere = 0;
-						double dCollapse = 0;
-
-	
-						if (isFind) {
-							array<String^>^ values = CSVFileManager::Parse(outputLineData, ",");
-
-							Double::TryParse(values[0], dPGA);
-							Double::TryParse(values[1], dSlight);
-							Double::TryParse(values[2], dModerate);
-							Double::TryParse(values[3], dSevere);
-							Double::TryParse(values[4], dCollapse);
-
-							fragilityFile = gcnew FragilityFile();
-							fragilityFile->FileName = sFragilityCurvFileName;
-							fragilityFile->PGA = dPGA;
-							fragilityFile->Slight = dSlight;
-							fragilityFile->Moderate = dModerate;
-							fragilityFile->Severe = dSevere;
-							fragilityFile->Collapse = dCollapse;
-
-
-							fragilityDataSet->m_FragilityFileDict->Add(dPGA, fragilityFile);
-						}
-
-						if (outputLineData->IndexOf("PGA") >= 0) {
-							isFind = true;
-						}
-					}
-				}
-
-
-				//Debug::WriteLine("=================>count:" + fragilityDataSet->m_FragilityFileDict->Count);
+				FragilityDataSet^ fragilityDataSet = gcnew FragilityDataSet(sFragilityCurvFileName);
+			
+				Debug::WriteLine("=================>count:" + fragilityDataSet->m_FragilityFileDict->Count);
 
 				
 				int samplingSize = fragilityDataSet->m_FragilityFileDict->Count;
